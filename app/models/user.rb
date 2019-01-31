db569bec181ef62dc1210c4c2b9df336c2c27c4b
Kaeland Chatman <kaeland1@gmail.com>
2019-01-31 13:13:12 -0500

    Brit fixed my User model self join error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   belongs_to :plant
   has_many :rounds, through: :shifts
-  has_many :operators, class_name: “User”, foreign_key: "manager_id"
-  belongs_to :manager, class_name: “User”
+  has_many :operators, class_name: "User", foreign_key: "manager_id"
+  belongs_to :manager, class_name: "User"
   has_one :address, as: :addressable
 end
