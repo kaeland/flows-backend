1c094a67a75f590999fec87794b14828dd9a07c8
Kaeland Chatman <kaeland1@gmail.com>
2019-02-01 12:45:23 -0500

    create a controller for the Datum model, with working Postman routes.

---
+++ app/controllers/api/v1/data_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::DataController < ApplicationController
+  skip_before_action :authorized
+  # skip_before_action :authorized, only: [:create]
+
+  def index
+    @data = Datum.all 
+    render json: @data, status: :ok
+  end
+
+  def show 
+    @datum = Datum.find(params[:id])
+    render json: @datum, status: :ok
+  end
+  
+  def create
+    @datum = Datum.create(datum_params)
+    render json: @datum, status: :ok  
+  end
+
+  def update
+    @datum = Datum.find(params[:id])
+    @datum.update(datum_params)
+    render json: @datum, status: :ok 
+  end
+
+  private
+  def datum_params
+    params.require(:datum).permit(
+      :name, 
+      :payload,
+      :machine_id
+    )
+  end
+end
