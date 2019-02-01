9d35f7e4c6e718f11a5a6c70dbaa8ce16e7a0e26
Kaeland Chatman <kaeland1@gmail.com>
2019-02-01 14:11:50 -0500

    created a controller for the Shift model, with working Postman routes.

---
+++ app/controllers/api/v1/shifts_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ShiftsController < ApplicationController
+  skip_before_action :authorized
+  # skip_before_action :authorized, only: [:create]
+
+  def index
+    @shifts = Shift.all 
+    render json: @shifts, status: :ok
+  end
+
+  def show 
+    @shift = Shift.find(params[:id])
+    render json: @shift, status: :ok
+  end
+  
+  def create
+    @shift = Shift.create(shift_params)
+    render json: @shift, status: :ok  
+  end
+
+  def update
+    @shift = Shift.find(params[:id])
+    @shift.update(shift_params)
+    render json: @shift, status: :ok 
+  end
+
+  private
+  def shift_params
+    params.require(:shift).permit(
+      :user_id, 
+      :round_id
+    )
+  end
+end
