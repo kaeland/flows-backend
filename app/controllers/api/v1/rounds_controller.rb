b42301f22953d86c799f0fd4b11b70a6a48c5384
Kaeland Chatman <kaeland1@gmail.com>
2019-02-01 12:15:24 -0500

    created a controller for the Round model, with working Postman routes

---
+++ app/controllers/api/v1/rounds_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::RoundsController < ApplicationController
+  skip_before_action :authorized
+  # skip_before_action :authorized, only: [:create]
+
+  def index
+    @rounds = Round.all 
+    render json: @rounds, status: :ok
+  end
+
+  def show 
+    @round = Round.find(params[:id])
+    render json: @round, status: :ok
+  end
+  
+  def create
+    @round = Round.create(round_params)
+    render json: @round, status: :ok  
+  end
+
+  def update
+    @round = Round.find(params[:id])
+    @round.update(round_params)
+    render json: @round, status: :ok 
+  end
+
+  private
+  def round_params
+    params.require(:round).permit(
+      :name, 
+      :time_of_day
+    )
+  end
+end
