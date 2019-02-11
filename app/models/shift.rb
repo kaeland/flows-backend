16e136da77596bc8d1fcc680f6c9474a13a22c71
Kaeland Chatman <kaeland1@gmail.com>
2019-02-11 16:13:03 -0500

    Added has_many :rounds association to the Shift model.

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,5 +2,8 @@ class Shift < ApplicationRecord
   has_many :user_shifts, dependent: :destroy
   has_many :users, through: :user_shifts
 
+  has_many :rounds, dependent: :destroy
+
   has_many :machine_rounds, dependent: :destroy
+  has_many :machines, through: :machine_rounds
 end
